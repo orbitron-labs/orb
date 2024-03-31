90f00b50eaf28fdc801d515331d750188c8618e6
ankit <ankitgci1@gmail.com>
2024-03-31 11:04:09 +0530

    install script update & config change

--- src/config.rs
+++ src/config.rs
@@ -6,10 +6,10 @@ use dirs::home_dir;
 
 pub fn load_config() -> TomlValue {
     // Specify the path to your config.toml file
-    let config_path = "config.toml";
-    // let config_path = home_dir()
-    //     .expect("Failed to determine the user's home directory")
-    //     .join("orb-p/config.toml");
+    // let config_path = "config.toml";
+    let config_path = home_dir()
+        .expect("Failed to determine the user's home directory")
+        .join(".orb/config.toml");
 
     // Read the content of the config file
     let mut file = File::open(config_path).expect("Failed to open config.toml");
