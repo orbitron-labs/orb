34ab481fe9811a23acb39129216dd541876ae1d7
ankit <ankitgci1@gmail.com>
2024-03-31 13:54:29 +0530

    avail ready

--- scripts/run/avail.sh
+++ scripts/run/avail.sh
@@ -4,9 +4,9 @@ set -e
 INTERNAL_DIR="/usr/local/bin"
 
 # check if the binary is already installed
-if [ -f "$INTERNAL_DIR/orb-avail-light" ]; then    
+if [ -f "$INTERNAL_DIR/avail-light" ]; then    
     echo "🚀 Avail is already installed" 
-    data-avail --dev --rpc-port 9945 --port 30334
+    avail-light --network goldberg
     exit 0
 fi
 
@@ -20,15 +20,14 @@ elif [[ "$ARCH" == "arm64" ]] || [[ "$ARCH" == "aarch64" ]]; then
 fi
 
 # Download avail binary
-TGZ_URL="https://github.com/orbitron-labs/orb/releases/download/avail-v1.7.9/avail-${OS}-${ARCH}.zip"
-sudo mkdir -p "/tmp/orbavail"
 echo "💈 Downloading Avail..."
-sudo curl -o /tmp/orbavail/${OS}_${ARCH}.zip -L "$TGZ_URL" --progress-bar
+TGZ_URL="https://github.com/orbitron-labs/orb/releases/download/avail-v1.7.9/avail-${OS}-${ARCH}.zip"
+curl -sLO "$TGZ_URL" --progress-bar
 
-sudo unzip -q /tmp/orbavail/${OS}_${ARCH}.zip -d /tmp/orb-avail-light/
-sudo mv "/tmp/orb-avail-light/${OS}_${ARCH}"/* "$INTERNAL_DIR"
-sudo chmod +x "$INTERNAL_DIR"
-sudo rm -rf "/tmp/orbavail"
+unzip -q avail-${OS}-${ARCH}.zip 
+chmod +x avail-light-linux-amd64
+sudo mv avail-light-linux-amd64 "$INTERNAL_DIR"/avail-light
+rm avail-${OS}-${ARCH}.zip
 
 # Handle this in GetStartCmd
-orb-avail-light --dev --rpc-port 9945 --port 30334
+avail-light --network goldberg
